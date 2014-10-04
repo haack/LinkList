24be20667b958c076ce0a5d264559ce4db8a966c
Alex <alex_haak@hotmail.co.uk>
2014-10-04 13:55:25 +0100

    Added basic hard-coded api

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var app = angular.module('app', []);
+
+app.controller('MainController', function($scope) {
+	$scope.message = "hello there";
+});
+
 $(function() {
 	console.log("Let's go...");
 });
